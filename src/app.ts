767eda08be1ba0df32fb511c09622695f3298ee0
Manik <tonysins43@gmail.com>
2024-07-16 16:16:55 +0600

    make a global error handler middleware for handle all error

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import express, { Application } from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-
+import {} from 'express';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
 const app: Application = express();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
+
+app.use(globalErrorHandler);
 
 export default app;
---
+++ src/app/middlewares/globalErrorHandler.ts
@@ -0,0 +1,18 @@
+import { ErrorRequestHandler } from 'express';
+
+// basic error handling -----------------------------------
+const globalErrorHandler: ErrorRequestHandler = (
+  err,
+  req,
+  res,
+  // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+  next,
+) => {
+  const statusCode = err.statusCode || 500;
+  const message = 'Something went wrong';
+  return res
+    .status(statusCode)
+    .json({ success: false, message: err.message || message, error: err });
+};
+
+export default globalErrorHandler;
