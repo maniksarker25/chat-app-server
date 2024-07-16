a5cd4c615987d2fbd385bb5e4346ff785e9af1ab
Manik <tonysins43@gmail.com>
2024-07-16 15:56:23 +0600

    make some changes

--- src/app/controllers/user.controller.ts
+++ src/app/controllers/user.controller.ts
@@ -1,8 +1,13 @@
-import User from '../models/user.model';
+import { NextFunction, Request, Response } from 'express';
+import { userServices } from '../services/user.service';
 
-const registerUser = async (req, res, next) => {
+const registerUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   try {
-    const result = await User.create(req.body);
+    const result = await userServices.registerUserIntoDB(req.body);
     res.status(200).json({
       success: true,
       message: 'User created successfully',
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,7 +1,11 @@
 import { IUser } from '../interfaces/user.interface';
+import User from '../models/user.model';
 
-const registerUserIntoDB = async (payload: IUser) => {};
+const registerUserIntoDB = async (payload: IUser) => {
+  const result = await User.create(payload);
+  return result;
+};
 
-export const registerServices = {
+export const userServices = {
   registerUserIntoDB,
 };
