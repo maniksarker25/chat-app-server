5868658557147f09a725302bb2a71043866fcee7
Manik <tonysins43@gmail.com>
2024-07-16 17:53:43 +0600

    create catchAsync utili function to handle try catch

--- src/app/controllers/user.controller.ts
+++ src/app/controllers/user.controller.ts
@@ -1,26 +1,18 @@
-import { NextFunction, Request, Response } from 'express';
 import { userServices } from '../services/user.service';
 import sendResponse from '../utils/sendResponse';
 import httpStatus from 'http-status';
+import catchAsync from '../utils/catchAsync';
 
-const registerUser = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const result = await userServices.registerUserIntoDB(req.body);
+const registerUser = catchAsync(async (req, res) => {
+  const result = await userServices.registerUserIntoDB(req.body);
 
-    sendResponse(res, {
-      statusCode: httpStatus.CREATED,
-      success: true,
-      message: 'User registered successfully',
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+  sendResponse(res, {
+    statusCode: httpStatus.CREATED,
+    success: true,
+    message: 'User registered successfully',
+    data: result,
+  });
+});
 
 export const userControllers = {
   registerUser,
