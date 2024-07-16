6b8499acbbd3be29a727034bd53d18d496f83c6f
Manik <tonysins43@gmail.com>
2024-07-16 16:44:48 +0600

    make send response fucntion for send response

--- src/app/controllers/user.controller.ts
+++ src/app/controllers/user.controller.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import { userServices } from '../services/user.service';
+import sendResponse from '../utils/sendResponse';
+import httpStatus from 'http-status';
 
 const registerUser = async (
   req: Request,
@@ -8,9 +10,11 @@ const registerUser = async (
 ) => {
   try {
     const result = await userServices.registerUserIntoDB(req.body);
-    res.status(200).json({
+
+    sendResponse(res, {
+      statusCode: httpStatus.CREATED,
       success: true,
-      message: 'User created successfully',
+      message: 'User registered successfully',
       data: result,
     });
   } catch (error) {
