6d1613cdc96c5b8b3a79c8cd3db59f140f50d996
Manik <tonysins43@gmail.com>
2024-07-16 15:26:15 +0600

    create message model

---
+++ src/app/models/message.model.ts
@@ -0,0 +1,30 @@
+import { model, Schema } from 'mongoose';
+import { IMessage } from '../interfaces/message.interface';
+
+const messageSchema = new Schema<IMessage>(
+  {
+    text: {
+      type: String,
+      default: '',
+    },
+    imageUrl: {
+      type: String,
+      default: '',
+    },
+    videoUrl: {
+      type: String,
+      default: '',
+    },
+    seen: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Message = model<IMessage>('message', messageSchema);
+
+export default Message;
